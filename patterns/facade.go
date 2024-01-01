36dac895ce4472f701a01a714cb1e7a289121cc9
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2024-01-01 14:36:42 +0600

    facade

---
+++ patterns/facade.go
@@ -0,0 +1,36 @@
+package patterns
+
+import "fmt"
+
+/*
+Паттерн "фасад" это некоторый объект, аккумулирующий в себе высокоуровневый набор операций 
+для работы с некоторой сложной подсистемой. 
+Фасад агрегирует классы, реализующие функциональность этой подсистемы, но не скрывает их. 
+Важно понимать, что клиент, при этом, не лишается более низкоуровневого доступа к классам подсистемы, 
+если ему это, конечно, необходимо. Фасад упрощает выполнение некоторых операций с подсистемой, 
+но не навязывает клиенту свое использование.
+*/
+
+//внешний тип wallet
+type Wallet struct {
+    accountId *int
+    holderName *string
+    balance *int
+}
+
+//фасад, дополняющий тип wallet
+type WalletFacade struct {
+	wallet Wallet
+}
+
+
+func (wf *WalletFacade) subtructMoneyFromWallet(accountID int, amount int) error {
+    if (accountID != *wf.wallet.accountId) {
+		return fmt.Errorf("wrong account id")
+	}
+	if (*wf.wallet.balance > amount) {
+		return fmt.Errorf("insufficent funds")
+	}
+	*wf.wallet.balance = *wf.wallet.balance - amount
+    return nil
+}
