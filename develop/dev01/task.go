53788e9be3f3245d339a9d39b5e15e4e3b682b3b
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2024-01-03 18:57:40 +0600

    ntptime

---
+++ develop/dev01/task.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+/*
+Создать программу печатающую точное время с использованием NTP -библиотеки.
+Инициализировать как go module. Использовать библиотеку github.com/beevik/ntp.
+Написать программу печатающую текущее время / точное время с использованием этой библиотеки.
+
+Программа должна быть оформлена как go module
+Программа должна корректно обрабатывать ошибки библиотеки: выводить их в STDERR и возвращать ненулевой код выхода в OS
+*/
+
+func main() {
+	fmt.Println(GetNTPTime())
+}
+
+func GetNTPTime() time.Time {
+	t, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(3)
+	}
+
+	return t
+}
